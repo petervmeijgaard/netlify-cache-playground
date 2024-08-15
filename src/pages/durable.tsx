cb21b854ffd95352ed06ae206b4d9a2208114928
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2024-08-15 10:33:54 +0200

    Add durable page

--- src/pages/durable.tsx
+++ src/pages/durable.tsx
@@ -3,18 +3,18 @@ import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 export function getServerSideProps({ res }: GetServerSidePropsContext) {
 	const randomNumber = Math.floor(Math.random() * 1000);
 
-	console.log("=== === BACKEND CALLED === ===");
+	console.log("=== === [DURABLE] BACKEND CALLED === ===");
 	console.log(randomNumber);
-	console.log("=== === BACKEND CALLED === ===");
+	console.log("=== === [DURABLE] BACKEND CALLED === ===");
 
 	res.setHeader(
 		"Cache-Control",
-		"public, max-age=120, stale-while-revalidate=60, stale-if-error=60",
+		"public, durable, max-age=120, stale-while-revalidate=60, stale-if-error=60",
 	);
 
 	res.setHeader(
 		"Netlify-CDN-Cache-Control",
-		"public, max-age=120, stale-while-revalidate=60, stale-if-error=60",
+		"public, durable, max-age=120, stale-while-revalidate=60, stale-if-error=60",
 	);
 
 	return {
@@ -27,5 +27,5 @@ export function getServerSideProps({ res }: GetServerSidePropsContext) {
 export default function Home({
 	randomNumber,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-	return <div>[Non-durable cache] Random number: {randomNumber}</div>;
+	return <div>[Durable cache] Random number: {randomNumber}</div>;
 }
