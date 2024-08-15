f7292efe320956dd831d97d1f71e8dc3a7201b89
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2024-08-15 09:48:29 +0200

    Use cache timings according to docs

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import { GetServerSidePropsContext } from "next";
 export function getServerSideProps({ res }: GetServerSidePropsContext) {
 	res.setHeader(
 		"Cache-Control",
-		"public, max-age=600, stale-while-revalidate=300, stale-if-error=300",
+		"public, max-age=300, stale-while-revalidate=600, stale-if-error=600",
 	);
 
 	res.setHeader(
 		"Netlify-CDN-Cache-Control",
-		"public, max-age=600, stale-while-revalidate=300, stale-if-error=300",
+		"public, max-age=300, stale-while-revalidate=600, stale-if-error=600",
 	);
 
 	return {
